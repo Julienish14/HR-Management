94ade492a8d7ae4008a81b93779f3566be072c2a
julien14 <juxishimwe@gmail.com>
2025-06-24 00:29:53 +0200

    final login page work

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import LoginPage from "./components/LoginPage";
 function App() {
-  return <div className="App">
-    <LoginPage
-  </div>;
+  return (
+    <div className="App">
+      <LoginPage />
+    </div>
+  );
 }
 
 export default App;
