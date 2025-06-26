52549bf0e5039bb155cd7beb89fd23629817c46b
julien14 <juxishimwe@gmail.com>
2025-06-26 16:31:43 +0200

    on login page and signup

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import AuthLayout from "../components/Auth/AuthLayout";
-import SignupForm from "../components/Auth/SignupForm";
+// import React from "react";
+// import AuthLayout from "../components/Auth/AuthLayout";
+// import SignupForm from "../components/Auth/SignupForm";
 
-const Signup: React.FC = () => {
-  return (
-    <AuthLayout
-      title="Login | Sign Up"
-      version="20.2.0.1"
-      copyright="2023-24 HRM and services"
-    >
-      <SignupForm />
-    </AuthLayout>
-  );
-};
+// const Signup: React.FC = () => {
+//   return (
+//     <AuthLayout
+//       title="Login | Sign Up"
+//       version="20.2.0.1"
+//     //   copyright="2023-24 HRM and services"
+//     >
+//       <SignupForm />
+//     </AuthLayout>
+//   );
+// };
 
-export default Signup;
+// export default Signup;
