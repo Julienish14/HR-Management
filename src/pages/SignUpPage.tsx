944000f69f99b74292734521ded71f25e1caef05
julien14 <juxishimwe@gmail.com>
2025-07-08 20:43:36 +0200

    work on signup button

--- src/pages/SignUpPage.tsx
+++ src/pages/SignUpPage.tsx
@@ -105,27 +105,15 @@ const SignUpPage = () => {
               Sign Up
             </button>
           </div>
+          <div className="mt-2">
+            <button
+              type="button"
+              className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
+            >
+              <span>Sign Up with Google</span>
+            </button>
+          </div>
         </form>
-
-        {/* <div className="relative">
-          <div className="absolute inset-0 flex items-center">
-            <div className="w-full border-t border-gray-300"></div>
-          </div>
-          <div className="relative flex justify-center text-sm">
-            <span className="px-2 bg-white text-gray-500">
-              Or continue with
-            </span>
-          </div>
-        </div> */}
-
-        <div className="mt-2">
-          <button
-            type="button"
-            className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
-          >
-            <span>Sign Up with Google</span>
-          </button>
-        </div>
 
         <div className="mt-8 text-center text-sm text-gray-500">
           <p>Rebecca Notes</p>
