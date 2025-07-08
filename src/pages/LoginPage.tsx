d94e23756a0bda36bd32f00879c9f5aa4cdec522
julien14 <juxishimwe@gmail.com>
2025-07-08 20:36:59 +0200

    work on button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -84,6 +84,8 @@ const LoginPage = () => {
             >
               Login
             </button>
+          </div>
+          <div>
             <button
               type="button"
               className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary"
