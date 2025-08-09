54ee3edb54d71b2dbfc5340da60278eefe784995
julien14 <juxishimwe@gmail.com>
2025-08-09 18:24:03 +0200

    signup;

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -59,13 +59,13 @@ export const LoginForm = () => {
               aria-hidden="true"
             />
           </div>
-          <input
+          {/* <input
             id="password"
             type="password"
             autoComplete="current-password"
             className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
             {...register("password", { required: "Password is required" })}
-          />
+          /> */}
         </div>
         {errors.password && (
           <p className="mt-2 text-sm text-red-600">{errors.password.message}</p>
