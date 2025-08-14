7a7ffb81bc901891942798f8241580d1379d3e36
julien14 <juxishimwe@gmail.com>
2025-08-15 00:11:49 +0200

    apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import LoginPage from "./pages/LoginPage";
-// import SignUpPage from "./pages/SignUpPage";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/login" element={<LoginPage />} />
-//         <Route path="/signup" element={<SignUpPage />} />
-//         <Route path="/" element={<LoginPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+}
 
-// export default App;
+export default App;
