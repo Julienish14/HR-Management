6c71639481bf4f32f8f16fd5dd1a513e40968693
julien14 <juxishimwe@gmail.com>
2025-08-26 23:54:12 +0200

    work on the login page inputs

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -33,21 +33,6 @@ const LoginPage = () => {
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
               />
             </div>
-            {/* <div>
-              <label
-                htmlFor="fullnames"
-                className="block text-sm font-medium text-gray-700"
-              >
-                fullnames
-              </label>
-              <input
-                id="fullnames"
-                name="fullnames"
-                type="text"
-                required
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
-              />
-            </div> */}
 
             <div>
               <label
