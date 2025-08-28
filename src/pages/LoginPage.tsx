9187d979127913e143d71e33151d13acaa798e9f
julien14 <juxishimwe@gmail.com>
2025-08-28 17:26:01 +0200

    work on the login page inputs

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -41,14 +41,14 @@ const LoginPage = () => {
               >
                 Password
               </label>
-              {/* <input
+              <input
                 id="password"
                 name="password"
                 type="password"
                 required
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
               />
-            </div> */}
+            </div>
 
             <div className="flex items-center justify-between">
               <div className="flex items-center">
