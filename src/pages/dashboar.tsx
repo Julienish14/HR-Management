1db70f365bf26f0a9030454c3381badc8686180c
julien14 <juxishimwe@gmail.com>
2025-09-20 22:12:01 +0200

    Add dash

---
+++ src/pages/dashboar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "danger" | "success";
+  size?: "sm" | "md" | "lg";
+}
