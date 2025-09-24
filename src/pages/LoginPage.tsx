7b1c65e19233a9b9c42e54e5e60fbbfe2a1ae3f4
julien14 <juxishimwe@gmail.com>
2025-09-24 14:41:46 +0200

    few changes on login pages...

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -25,13 +25,13 @@ const LoginPage = () => {
               >
                 Username
               </label>
-              {/* <input
+              <input
                 id="username"
                 name="username"
                 type="text"
                 required
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-primary focus:border-primary"
-              /> */}
+              />
             </div>
 
             <div>
