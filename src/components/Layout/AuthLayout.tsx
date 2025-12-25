3e77b1a8787633b2cefa21309f134aba478b56a7
julien14 <juxishimwe@gmail.com>
2025-12-25 21:25:14 +0200

    coding

--- src/components/Layout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout.tsx
@@ -70,3 +70,38 @@ export const Input: React.FC<InputProps> = ({
     </div>
   );
 };
+
+import Logo from "@/assets/Logo.png";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary-100 py-16">
+      <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
+        <div className="mt-16 basis-1/2 md:mt-0">
+          <img alt="logo" src={Logo} />
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Links</h4>
+
+          <p className="my-5">
+            {" "}
+            <a
+              href="https://x.com/___jish"
+              target="_blank"
+              //   rel="noopener noreferrer"
+            >
+              X(twitter)
+            </a>{" "}
+          </p>
+        </div>
+        <div className="mt-16 basis-1/4 md:mt-0">
+          <h4 className="font-bold">Contact Us</h4>
+          <p className="my-5">natus quidem eveniet</p>
+          <p>(334)434-6894</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
