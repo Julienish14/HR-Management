8b6466fec2ab2c63b7fa522c1cf258c48cb1e378
julien14 <juxishimwe@gmail.com>
2025-12-30 15:57:15 +0200

    coding

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import LoginPage from "./pages/LoginPage";
 // import SignUpPage from "./pages/SignUpPage";
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/" element={<LoginPage />} />
-      </Routes>
-    </Router>
-  );
-}
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/login" element={<LoginPage />} />
+//         <Route path="/signup" element={<SignUpPage />} />
+//         <Route path="/" element={<LoginPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
