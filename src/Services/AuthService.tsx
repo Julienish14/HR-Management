a67462be7a38e8039cc9a16a1cef0291c1faebf9
julien14 <juxishimwe@gmail.com>
2026-01-01 14:45:29 +0200

    add services to the application

---
+++ src/Services/AuthService.tsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = "http://localhost:5167/api";
+
+export const loginAPI = async (username: string, password: string) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + "account/login", {
+      username: username,
+      password: password,
+    });
+  } catch (error) {
+    handleError();
+  }
+};
