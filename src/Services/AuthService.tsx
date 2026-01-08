17619088b197a559c41edbe3e77e411a264d555d
julien14 <juxishimwe@gmail.com>
2026-01-08 14:04:37 +0200

    create a register API

--- src/Services/AuthService.tsx
+++ src/Services/AuthService.tsx
@@ -15,3 +15,20 @@ export const loginAPI = async (username: string, password: string) => {
     handleError(error);
   }
 };
+
+export const registerAPI = async (
+  email: string,
+  username: string,
+  password: string
+) => {
+  try {
+    const data = await axios.post<UserProfileToken>(api + "account/register", {
+      email: email,
+      username: username,
+      password: password,
+    });
+    return data;
+  } catch (error) {
+    handleError(error);
+  }
+};
