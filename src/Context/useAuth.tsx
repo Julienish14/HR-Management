92893599efeb9209daff9d22b897f0ec52dfda3c
julien14 <juxishimwe@gmail.com>
2026-01-09 14:00:24 +0200

    working on userAuth

--- src/Context/useAuth.tsx
+++ src/Context/useAuth.tsx
@@ -1,5 +1,10 @@
-export type UserProfileToken = {
-  userName: string;
-  email: string;
-  token: string;
+import { UserProfile } from "../Models/User";
+
+type UserContextType = {
+  user: UserProfile | null;
+  token: string | null;
+  registerUser: (email: string, username: string, password: string) => void;
+  loginUser: (username: string, password: string) => void;
+  logoutUser: () => void;
+  isLoggedIn: () => boolean;
 };
