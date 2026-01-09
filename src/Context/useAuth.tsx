0a16907865c20a2efa043c05bdff6b7489dc06c0
julien14 <juxishimwe@gmail.com>
2026-01-09 14:20:20 +0200

    register user context

--- src/Context/useAuth.tsx
+++ src/Context/useAuth.tsx
@@ -1,4 +1,8 @@
+import { createContext, useEffect, useState } from "react";
 import { UserProfile } from "../Models/User";
+import { useNavigate } from "react-router-dom";
+import { registerAPI } from "../Services/AuthService";
+import { toast } from "react-toastify";
 
 type UserContextType = {
   user: UserProfile | null;
@@ -7,4 +11,48 @@ type UserContextType = {
   loginUser: (username: string, password: string) => void;
   logoutUser: () => void;
   isLoggedIn: () => boolean;
+};
+
+type Props = { children: React.ReactNode };
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
+
+export const UserProvider = ({ children }: Props) => {
+  const navigate = useNavigate();
+  const [token, setToken] = useState<string | null>(null);
+  const [user, setUser] = useState<UserProfile | null>(null);
+  const [isReady, setIsReady] = useState<boolean>(false); //we can use useState(false)
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    if (user && token) {
+      setUser(JSON.parse(user));
+      setToken(token);
+    }
+    setIsReady(true);
+  }, []);
+
+  const register = async (
+    email: string,
+    username: string,
+    password: string
+  ) => {
+    await registerAPI(email, username, password)
+      .then((res) => {
+        if (res) {
+          localStorage.setItem("token", res?.data.token);
+          const userObj = {
+            userName: res?.data.username,
+            email: res?.data.email,
+          };
+          localStorage.setItem("user", JSON.stringify(userObj));
+          setToken(res?.data.token!);
+          setUser(userObj!);
+          toast.success("Registration successful!");
+          navigate("/dashboard");
+        }
+      })
+      .catch((e) => toast.warning("Server error occurred"));
+  };
 };
