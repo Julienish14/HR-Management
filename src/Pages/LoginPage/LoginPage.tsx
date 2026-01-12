b3b35040cb144b79e098452fa8b52a3be76cd8d9
julien14 <juxishimwe@gmail.com>
2026-01-12 13:53:49 +0200

    logginginnnn

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -36,7 +36,10 @@ const LoginPage = (props: Props) => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form
+              className="space-y-4 md:space-y-6"
+              onSubmit={handleSubmit(handleLogin)}
+            >
               <div>
                 <label
                   htmlFor="email"
@@ -46,11 +49,12 @@ const LoginPage = (props: Props) => {
                 </label>
                 <input
                   type="text"
-                  name="username"
                   id="username"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Username"
+                  {...register("username")}
                 />
+                {errors.username ? <p>{errors.username.message}</p> : ""}
               </div>
               <div>
                 <label
