eb86f49c28c21cc32652cb0ef1f42b66dda4c173
julien14 <juxishimwe@gmail.com>
2026-01-13 14:24:12 +0200

    Final fix of the tailwind issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@
 //   plugins: [tailwindcss()],
 // });
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
+  plugins: [react(), tailwindcss()],
 });
