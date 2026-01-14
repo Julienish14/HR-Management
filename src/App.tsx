2efdef67c8a90be87f7562dced193c717f3905b6
julien14 <juxishimwe@gmail.com>
2026-01-14 13:41:57 +0200

    runaway

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,66 @@
-import { Outlet } from "react-router";
-import "react-toastify/dist/ReactToastify.css";
-import "./App.css";
-import { ToastContainer } from "react-toastify";
-import { UserProvider } from "./Context/useAuth";
+// // import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// // import LoginPage from "./pages/LoginPage";
+// // import SignUpPage from "./pages/SignUpPage";
+
+// import { Outlet } from "react-router";
+// import "react-toastify/dist/ReactToastify.css";
+// import "./App.css";
+// import { ToastContainer } from "react-toastify";
+// import { UserProvider } from "./Context/useAuth";
+// import AppRoutes from "./Routes/Routes";
+
+// function App() {
+//   return (
+//     <>
+//       <UserProvider>
+//         {/* <Navbar /> */}
+//         <Outlet />
+//         <ToastContainer />
+//       </UserProvider>
+//       <AppRoutes />
+//     </>
+//     // <Router>
+//     //   <Routes>
+//     //     <Route path="/login" element={<LoginPage />} />
+//     //     {/* <Route path="/signup" element={<SignUpPage />} /> */}
+//     //     {/* <Route path="/" element={<LoginPage />} /> */}
+//     //   </Routes>
+//     // </Router>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
 import AppRoutes from "./Routes/Routes";
 
-const App: React.FC = () => {
-  return <AppRoutes />;
+const App = () => {
+  return (
+    <div className="min-h-screen bg-red-50">
+      <AppRoutes />
+    </div>
+  );
 };
 
 export default App;
 
 // import React from "react";
+// import { Outlet } from "react-router";
+// import "react-toastify/dist/ReactToastify.css";
+// import "./App.css";
+// import { ToastContainer } from "react-toastify";
+// import { UserProvider } from "./Context/useAuth";
 // import AppRoutes from "./Routes/Routes";
 
 // const App: React.FC = () => {
 //   return (
 //     <div className="min-h-screen bg-red-50">
 //       <AppRoutes />
+//       <UserProvider>
+//         {/* <Navbar /> */}
+//         <Outlet />
+//         <ToastContainer />
+//       </UserProvider>
 //     </div>
 //   );
 // };
