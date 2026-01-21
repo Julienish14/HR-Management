0a95cc30d7c25da340ce25c27122b7f132b2214e
julien14 <juxishimwe@gmail.com>
2026-01-21 15:31:55 +0200

    trobleshoot loginUser is not function

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,36 +1,3 @@
-// import React from "react";
-// import * as Yup from "yup";
-// import { userAuth } from "../../Context/useAuth";
-// import { yupResolver } from "@hookform/resolvers/yup";
-// import { useForm } from "react-hook-form";
-
-// type Props = {};
-
-// type LoginFormsInputs = {
-//   email: string;
-//   // username: string;
-//   password: string;
-// };
-
-// const validation = Yup.object().shape({
-//   email: Yup.string()
-//     .required("Email is required")
-//     .email("Invalid email format"),
-//   // username: Yup.string().required("Username is required"),
-//   password: Yup.string().required("Password is required"),
-// });
-
-// const LoginPage = (props: Props) => {
-//   const { loginUser } = userAuth();
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm<LoginFormsInputs>({ resolver: yupResolver(validation) });
-
-//   const handleLogin = (form: LoginFormsInputs) => {
-//     loginUser(form.email, form.password);
-//   };
 import React from "react";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
