05c84fbad083efaaef8fbe387cbdd841ce263c6b
julien14 <juxishimwe@gmail.com>
2026-01-24 18:46:34 +0200

    re work on the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,38 +31,38 @@
 
 // export default App;
 
-import React from "react";
-import AppRoutes from "./Routes/Routes";
-
-const App: React.FC = () => {
-  return (
-    <div className="min-h-screen bg-red-50">
-      <AppRoutes />
-    </div>
-  );
-};
-
-export default App;
-
 // import React from "react";
-// import { Outlet } from "react-router";
-// import "react-toastify/dist/ReactToastify.css";
-// import "./App.css";
-// import { ToastContainer } from "react-toastify";
-// import { UserProvider } from "./Context/useAuth";
 // import AppRoutes from "./Routes/Routes";
 
 // const App: React.FC = () => {
 //   return (
 //     <div className="min-h-screen bg-red-50">
 //       <AppRoutes />
-//       <UserProvider>
-//         {/* <Navbar /> */}
-//         <Outlet />
-//         <ToastContainer />
-//       </UserProvider>
 //     </div>
 //   );
 // };
 
 // export default App;
+
+import React from "react";
+import { Outlet } from "react-router";
+import "react-toastify/dist/ReactToastify.css";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./Context/useAuth";
+import AppRoutes from "./Routes/Routes";
+
+const App: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-red-50">
+      <AppRoutes />
+      <UserProvider>
+        {/* <Navbar /> */}
+        <Outlet />
+        <ToastContainer />
+      </UserProvider>
+    </div>
+  );
+};
+
+export default App;
