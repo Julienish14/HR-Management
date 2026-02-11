fb466bd8e3ef704638ac9a0b4808155925045731
julien14 <juxishimwe@gmail.com>
2026-02-11 13:06:35 +0200

    fix

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -13,7 +13,7 @@ type LoginFormsInputs = {
 };
 
 const validation = Yup.object().shape({
-  // userName: Yup.string().required("Username is required"),
+  userName: Yup.string().required("Username is required"),
   email: Yup.string().required("Username is required"),
   password: Yup.string().required("Password is required"),
 });
