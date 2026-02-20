e8cfc37c65e83a58e32a601cf733890fdd478ba4
julien14 <juxishimwe@gmail.com>
2026-02-20 13:59:12 +0200

    authlayout

--- src/components/Layout/AuthLayout.tsx
+++ src/components/Layout/AuthLayout.tsx
@@ -1,18 +1,18 @@
-//   const auth = useAuth();
-//   console.log('Auth object:', auth); // Add this to debug
+  const auth = useAuth();
+  console.log('Auth object:', auth); // Add this to debug
 
-//   // Check if loginUser exists
-//   const loginUser = auth.loginUser || (() => {
-//     console.error('loginUser is not available');
-//     return Promise.reject('loginUser is not available');
-//   });
+  // Check if loginUser exists
+  const loginUser = auth.loginUser || (() => {
+    console.error('loginUser is not available');
+    return Promise.reject('loginUser is not available');
+  });
 
-//   const {
-//     register,
-//     handleSubmit,
-//     formState: { errors },
-//   } = useForm<LoginFormsInputs>({ resolver: yupResolver(validation) });
+  const {
+    register
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormsInputs>({ resolver: yupResolver(validation) });
 
-//   const handleLogin = (form: LoginFormsInputs) => {
-//     loginUser(form.email, form.password);
-//   };
+  const handleLogin = (form: LoginFormsInputs) => {
+    loginUser(form.email, form.password);
+  };
