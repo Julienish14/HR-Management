8612f2f4fff8c71c9dcf3da1d12df2e4540aca9c
julien14 <juxishimwe@gmail.com>
2026-03-02 22:17:09 +0200

    register

--- src/Pages/RegisterPage/RegisterPage.tsx
+++ src/Pages/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import * as Yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useAuth } from "../../Context/useAuth";
+import { useForm } from "react-hook-form";
 
-function () {
-  return <div></div>;
-}
+type Props = {};
 
-export default 
+type RegisterFormsInputs = {
+  email: string;
+  userName: string;
+  password: string;
+};
+
+const validation = Yup.object().shape({
+  email: Yup.string().required("Email is required"),
+  userName: Yup.string().required("Username is required"),
+  password: Yup.string().required("Password is required"),
+});
+
+const RegisterPage = (props: Props) => {
+  const { registerUser } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormsInputs>({ resolver: yupResolver(validation) });
+
+  const handleLogin = (form: RegisterFormsInputs) => {
+    registerUser(form.email, form.userName, form.password);
+  };
+  
+
+export default RegisterPage;
