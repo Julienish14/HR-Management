47614bf58d0cf94d1a4285932046a2b3cc62832a
julien14 <juxishimwe@gmail.com>
2026-03-09 19:55:47 +0200

    profile service

---
+++ src/Services/ProfileService.tsx
@@ -0,0 +1,5 @@
+import axios from "axios";
+import { PortfolioGet, PortfolioPost } from "../Models/Portfolio";
+import { handleError } from "../Helpers/ErrorHandler";
+
+const api = "http://localhost:5167/api/portfolio/";
