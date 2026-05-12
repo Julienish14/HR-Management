54bafd372cf700dea5c4be5ec6fc679e89ad2c54
julien14 <juxishimwe@gmail.com>
2026-05-12 08:15:26 +0200

    work on reg

--- src/Pages/RegisterPage/RegisterPage.tsx
+++ src/Pages/RegisterPage/RegisterPage.tsx
@@ -48,13 +48,13 @@ const RegisterPage = (props: Props) => {
                 >
                   Email
                 </label>
-                {/* <input
+                <input
                   type="text"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                   placeholder="Email"
                   {...register("email")}
-                /> */}
+                />
                 {errors.email ? (
                   <p className="text-white">{errors.email.message}</p>
                 ) : (
