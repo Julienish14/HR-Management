06a9c87e5964dba0ff44972376ceec7252cb7be0
julien14 <juxishimwe@gmail.com>
2026-06-02 21:16:41 +0200

    register page work

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -6,12 +6,14 @@ import {
   Navigate,
 } from "react-router-dom";
 import LoginPage from "../Pages/LoginPage/LoginPage";
+import RegisterPage from "../Pages/RegisterPage/RegisterPage";
 const AppRoutes: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/register" element={RegisterPage} />
       </Routes>
     </Router>
   );
