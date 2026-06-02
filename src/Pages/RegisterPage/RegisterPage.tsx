944669d380ab05bc4070b14da6bdad6d05f7d20a
julien14 <juxishimwe@gmail.com>
2026-06-02 21:42:34 +0200

    register page work

--- src/Pages/RegisterPage/RegisterPage.tsx
+++ src/Pages/RegisterPage/RegisterPage.tsx
@@ -111,7 +111,8 @@ const RegisterPage = (props: Props) => {
               </div>
               <button
                 type="submit"
-                className="w-full text-white bg-lightGreen hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                className="w-full py-3 px-4 bg-red hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700 text-white font-medium rounded-lg focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800 focus:outline-none transition-colors shadow-md hover:shadow-lg"
+                aria-label="Sign in to account"
               >
                 Sign in
               </button>
