0a3573833108e2481c403904a3b0121cff0f7260
julien14 <juxishimwe@gmail.com>
2026-06-03 21:27:44 +0200

    register page work

--- src/Pages/RegisterPage/RegisterPage.tsx
+++ src/Pages/RegisterPage/RegisterPage.tsx
@@ -52,7 +52,7 @@ const RegisterPage = (props: Props) => {
                   type="text"
                   id="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="Email"
+                  placeholder="Full name"
                   {...register("email")}
                 />
                 {errors.email ? (
@@ -142,7 +142,7 @@ const RegisterPage = (props: Props) => {
                   href="#"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                 >
-                  Sign up
+                  {/* Sign up */}
                 </a>
               </p>
             </form>
