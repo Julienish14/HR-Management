9512621860735f847d30ed05f3c333c570689572
julien14 <juxishimwe@gmail.com>
2026-07-07 22:06:33 +0200

    dashboard startcards

--- src/components/Auth/StatsCard.tsx
+++ src/components/Auth/StatsCard.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 
+
 const StatsCard: React.FC<> = ({
- 
+  title,
+  value,
+  change,
+  icon,
 }) => {
-  
+  const isPositive = change.startsWith("+");
+  const changeColor = isPositive ? "positive" : "negative";
 
 
 
