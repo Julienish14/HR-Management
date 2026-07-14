d3993a984bfda7150e040444b28db091f37f0229
julien14 <juxishimwe@gmail.com>
2026-07-15 00:45:13 +0200

    dashboard

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -27,5 +27,20 @@ const StatsCard: React.FC<StatsCardProps> = ({
     </div>
   );
 };
+const RecentActivity: React.FC<RecentActivityProps> = ({ activities }) => {
+  return (
+    <div className="activity-list">
+      {activities.map((activity) => (
+        <div key={activity.id} className="activity-item">
+          <div className="activity-content">
+            <span className="activity-user">{activity.user}</span>
+            <span className="activity-action">{activity.action}</span>
+          </div>
+          <span className="activity-time">{activity.time}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default StatsCard;
