c2a2d4c52316d4139b2d5e0de2cf7258b45c7c49
julien14 <juxishimwe@gmail.com>
2026-07-16 21:12:02 +0200

    dashboard

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React from "react";
 
+interface RecentActivityProps {}
 
-
-interface RecentActivityProps {
-}
-
-const RecentActivity: React.FC<RecentActivityProps> = ({  }) => {
- 
+const RecentActivity: React.FC<RecentActivityProps> = ({ activities }) => {
+  return (
+    <div className="activity-list">
+      {activities.map((activity) => (
+        <div key={activity.id} className="activity-item">
+          <span className="activity-time">{activity.time}</span>
+        </div>
+      ))}
+    </div>
   );
 };
 
